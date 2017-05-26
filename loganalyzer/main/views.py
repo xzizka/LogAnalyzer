e1f403797e036aa5e32a0e950834eec8315495a8
xzizka <xlu@seznam.cz>
2017-05-26 18:57:22 +0100

    Finished splitting to the blueprints + design changes

--- loganalyzer/main/views.py
+++ loganalyzer/main/views.py
@@ -4,21 +4,31 @@ from . import main
 from .. import login_manager
 from ..models import User, SupportBundle, Tag
 
+
 @login_manager.user_loader
 def load_user(userid):
     return User.query.get(int(userid))
+
 
 @main.route('/')
 def index():
     return render_template('index.html', new_sbs=SupportBundle.newest(5))
 
+
+@main.route('contact')
+def contact():
+    return render_template('contact.html')
+
+
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @main.app_errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
 
 @main.app_context_processor
 def inject_tags():
